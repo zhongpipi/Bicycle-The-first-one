a358c1aea2d722a56edadccbffb5eafb74760af7
zhongpipi <zhong_sufan@foxmail.com>
2020-05-19 00:10:36 +0800

    pytest

--- list_pack.py
+++ list_pack.py
@@ -8,6 +8,7 @@ def pack1(a, b, *c, **d):
 score = [1.0, 2.0, 3.0, 4.0, 5.1, 6.7]
 pack1(*score)
 
+
 def pack(*c):
     print(type(c), c)
 
@@ -15,11 +16,13 @@ def pack(*c):
 student = {'score': 1.0, 'id': 2, 'name': 'xiaoxiao'}
 pack(*student)
 
-def func(a,b,c):
-    print(a,b,c)
 
-func(*[1,2,3])
-func(*(1,2,3))
+def func(a, b, c):
+    print(a, b, c)
+
+
+func(*[1, 2, 3])
+func(*(1, 2, 3))
 func(*"abc")
-func(*{'a':1,'b':2,'c':3})
-func(**{'a':1,'b':2,'c':3})
+func(*{'a': 1, 'b': 2, 'c': 3})
+func(**{'a': 1, 'b': 2, 'c': 3})
